0f1658411cef4492217cb9b0712550967271085d
hariom-techculture <techcultureitsolutions@gmail.com>
2025-08-26 16:09:36 +0530

    add cors restriction

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ import dashboardRouter from "./routes/dashboardRoutes.js"
 
 const app = express()
 const PORT = process.env.PORT || 5000
-app.use(cors("*"))
+app.use(
+  cors({
+    origin: [
+      "http://appcenter.techculture.ai",
+      "http://techculture.ai",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://192.168.1.228:3000"
+    ],
+    methods:["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-access-token"],
+  })
+);
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 connectDB()
